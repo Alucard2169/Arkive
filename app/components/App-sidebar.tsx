8a1029b98a2620807990305fc304d8c63b577821
Alucard2169 <alucardd2169@gmail.com>
2025-04-08 13:39:34 +0530

    update sidebar

--- app/components/App-sidebar.tsx
+++ app/components/App-sidebar.tsx
@@ -1,146 +1,24 @@
-import { Book, Bookmark, Calendar, ChevronUp, Home, Inbox, LogOut, Search, Settings, Star, User, User2 } from "lucide-react"
 
 import {
   Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarFooter,
+  SidebarMenu,
+  SidebarMenuItem
 } from "@/components/ui/sidebar"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
 
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Bookmarks",
-    url: "#",
-    icon: Bookmark,
-  },
-  {
-    title: "Github Stars",
-    url: "#",
-    icon: Star,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
-
+import SidebarTop from "./SidebarTop"
+import SidebarDropMenu from "./SidebarDropMenu"
 
 
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel className="text-lg font-bold">Arkive</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+      <SidebarTop/>
       <SidebarFooter>
           <SidebarMenu>
             <SidebarMenuItem >
-            <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-      <SidebarMenuButton>
-                    <User2 /> Username
-                    <ChevronUp className="ml-auto" />
-                  </SidebarMenuButton>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Profile</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Keyboard shortcuts
-
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem>Email</DropdownMenuItem>
-                <DropdownMenuItem>Message</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>More...</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuItem>
-            New Team
-       
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>GitHub</DropdownMenuItem>
-        <DropdownMenuItem>Support</DropdownMenuItem>
-        <DropdownMenuItem disabled>API</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            <SidebarDropMenu/>
             </SidebarMenuItem>
           </SidebarMenu>
         </SidebarFooter>
